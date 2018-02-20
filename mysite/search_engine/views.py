af59ce286e6df9c8e0f13f05b1326f4c04d84eb2
Yidan Zhang <zhangyd@umich.edu>
2018-02-19 20:12:23 -0800

    added form and package cards

--- mysite/search_engine/views.py
+++ mysite/search_engine/views.py
@@ -4,7 +4,25 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
 	return render(request, 'search_engine/index.html')
+
 def results(request):
+	# get the search input (so we can use it later)
+	searchInput = request.GET.get('searchInput')
+
+	#making up temp values for packages
+	packages = []
+	for i in range(5):
+		package = {}
+		package['name'] = "my_name"
+		package['rating'] = 5
+		package['response_time'] = "2hrs"
+		package['license'] = "MIT"
+		package['published'] = "2014-03-12"
+		package['last_modified'] = "2016-07-21"
+		package['num_contributor'] = "10"
+		package['size'] = "2MB"
+		package['dependencies'] = "None"
+		packages.append(package)
 
 	#lets just say results look like this.
 	cicero_lorem_ipsum = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo."
@@ -15,7 +33,7 @@ def results(request):
 		result['url']   = 'www.reallylongurl.com'
 		result['desc']  = cicero_lorem_ipsum
 		results.append(result)
-	return render(request, 'search_engine/results_page.html', {'results': results})
+	return render(request, 'search_engine/results_page.html', {'results': results, 'packages': packages})
 
 
 
