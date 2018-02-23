f92a73f8e78c84c98d436726688dcacc264da8f0
Daniel Avery Nisbet <danisbet95@gmail.com>
2018-02-23 13:56:56 -0800

    dynamically display the chart

--- mysite/search_engine/static/search_engine/results.js
+++ mysite/search_engine/static/search_engine/results.js
@@ -10,7 +10,26 @@ $(document).ready(function() {
 
 	$('.package-button').click(function(e){
 		var package_name = $(this).attr('id');
-	        $(this).html(package_name);
+		var all_none = false;
+
+
+
+	    $('.'+package_name+'-table').each(function() {
+	    	$(this).toggle();
+	    });
+
+		var count = 0;
+		$('.table-header').each(function() {
+	    	if ($(this).is(":visible")) {
+	    		count++;
+	    	}
+
+	    	// if ($(this).find('td:empty')) { alert('one empty')}
+	    });
+
+	    if (count <= 1){
+	    	$('.package-table').toggle();
+	    }
     });
 
 });
