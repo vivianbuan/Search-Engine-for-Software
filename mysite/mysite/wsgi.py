19d0bee2e99348edcc9ab32f3c492322418ca06b
root <root@djangostack-1-vm.c.sturdy-quarter-195800.internal>
2018-03-01 22:56:21 +0000

    apache configuration to get it to show on the server

--- mysite/mysite/wsgi.py
+++ mysite/mysite/wsgi.py
@@ -8,8 +8,11 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
+
+sys.path.append('/opt/bitnami/apps/django/django_projects/Search-Engine-for-Software/mysite')
+os.environ.setdefault("PYTHON_EGG_CACHE", "/opt/bitnami/apps/django/django_projects/Search-Engine-for-Software/mysite/egg_cache")
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
