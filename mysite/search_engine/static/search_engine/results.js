169f7d97e52f5f675621afbb016843923ac51ece
Daniel Avery Nisbet <danisbet95@gmail.com>
2018-04-26 14:07:20 -0700

    same ^

--- mysite/search_engine/static/search_engine/results.js
+++ mysite/search_engine/static/search_engine/results.js
@@ -81,7 +81,6 @@ $(document).ready(function() {
 						$('.'+package_num+'-information-card').toggle();
 						// $(this).toggleClass('package-selected')
 						// $(this).toggleClass('package-unselected')
-						$('.'+package_num+'-information-card').toggle();
 					}
 
 				} else {
